f6c7bf785e883581d0db774476a57b027ccd92eb
Jake Farrell <jake_farrell@outlook.com>
2024-03-26 11:42:09 +0000

    finished assignment 5

--- week-10/JarJarBinks.java
+++ week-10/JarJarBinks.java
@@ -29,45 +29,42 @@ class StarWarsCharacter {
 
     // toString to print character info.
     public String toString() {
-        try {
-            
-
-        List<StarWarsCharacter> charactersList = new ArrayList<>();
-        Reader in = new FileReader("characters.csv");
-
-        CSVFormat CSVparser = CSVFormat.Builder.create().setHeader().build();
-
-        Iterable<CSVRecord> records = CSVparser.parse(in);
-        for (CSVRecord record : records) {
-            StarWarsCharacter character = new StarWarsCharacter();
-            character.name = record.get("name");
-            character.height = record.get("height");
-            character.mass = record.get("mass");
-            character.hairColor = record.get("hairColor");
-            character.skinColor = record.get("skinColor");
-            character.eyeColor = record.get("eyeColor");
-            character.birthYear = record.get("birthYear");
-            character.gender = record.get("gender");
-            character.homeworld = record.get("homeworld");
-            character.species = record.get("species");
-        }
-
-        Writer out = new FileWriter("characters.json");
-        Gson gson = new Gson();
-        gson.toJson(charactersList, out);
-        out.close();
-
-        return "Done";
-    } catch (Exception e) {
-        // TODO: handle exception
-    }
-        return "Done";
+        return this.name;
     }
 }
 
 public class JarJarBinks {
     public static void main(String[] args) {
-        StarWarsCharacter sw = new StarWarsCharacter();
-        System.out.println(sw);
+        try {
+            List<StarWarsCharacter> charactersList = new ArrayList<>();
+            Reader in = new FileReader("characters.csv");
+    
+            CSVFormat CSVparser = CSVFormat.Builder.create().setHeader().build();
+    
+            Iterable<CSVRecord> records = CSVparser.parse(in);
+            for (CSVRecord record : records) {
+                StarWarsCharacter character = new StarWarsCharacter();
+                character.name = record.get("name");
+                character.height = record.get("height");
+                character.mass = record.get("mass");
+                character.hairColor = record.get("hair_color");
+                character.skinColor = record.get("skin_color");
+                character.eyeColor = record.get("eye_color");
+                character.birthYear = record.get("birth_year");
+                character.gender = record.get("gender");
+                character.homeworld = record.get("homeworld");
+                character.species = record.get("species");
+                charactersList.add(character);
+            }
+    
+            Writer out = new FileWriter("characters.json");
+            Gson gson = new Gson();
+            gson.toJson(charactersList, out);
+            out.close();
+        } catch (Exception FileNotFoundException) {
+            System.err.println(FileNotFoundException);
+        }
+           
+    
     }
 }
